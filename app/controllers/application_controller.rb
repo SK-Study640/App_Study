86767e8012d72b53119d660eaf9bc11065753669
SK-Study640 <m9bp67nkma@gmail.com>
2024-09-21 18:41:46 +0900

    Create Home and introduce authentication with device gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller?  # Added to avoid system errors by ensuring Devise helper methods run only in Devise controllers.
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
+
