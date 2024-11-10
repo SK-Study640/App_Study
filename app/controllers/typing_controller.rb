b6e6b983637695c2d66e9be9c421e519c2e5a8fa
SK-Study640 <m9bp67nkma@gmail.com>
2024-11-10 12:40:16 +0900

    Completion of a typing function.

--- app/controllers/typing_controller.rb
+++ app/controllers/typing_controller.rb
@@ -1,9 +1,48 @@
 class TypingController < ApplicationController
+  require 'time'
   def home
     @results = Typing::Result.order(time: :asc)
   end
 
-  def play
+  def start
+    session[:start_time] = Time.now
+    session[:correct_count] = 0
+    redirect_to action: :play
+  end
 
+  def play
+    @sentence = Typing::Sentence.order("RANDOM()").first
+    @elapsed_time = Time.at(Time.now - Time.parse(session[:start_time])).to_i
+    @correct_count = session[:correct_count]
+  end
+
+  def check_answer
+    @sentence = Typing::Sentence.find(params[:sentence_id])
+    @user_input = params[:user_input]
+    @correct_count = session[:correct_count] || 0
+
+    if @sentence.content == @user_input
+      # 正解の場合
+      session[:correct_count] = @correct_count + 1
+      flash[:typing_notice] = "正解です"
+      #  10回以上正解したら結果画面へ
+      if session[:correct_count] >= 10
+        redirect_to action: :result
+        return
+      end
+    else
+      # 不正解の場合
+      flash[:typing_alert] = "不正解です"
+    end
+    redirect_to action: :play
+  end
+
+  def result
+    @elapsed_time = Time.at(Time.now - Time.parse(session[:start_time])).to_i
+    if Typing::Result.create_or_update(current_user, @elapsed_time)
+      flash[:typing_notice] = "ベストタイムを更新しました"
+    else
+      flash[:typing_notice] = "タイムは更新されませんでした"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_063003) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_20_074058) do
   create_table "typing_results", force: :cascade do |t|
     t.integer "time"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_typing_results_on_user_id"
+  end
+
+  create_table "typing_sentences", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
